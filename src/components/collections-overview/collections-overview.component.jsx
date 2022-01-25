828ac7535b7a3eea2bca80405740ced1abcec7c1
emylimas <ernestas.mylimas@1clickfactory.com>
2022-01-25 21:02:14 +0200

    moved out from css to js styles

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -6,7 +6,7 @@ import CollectionPreview  from '../collection-preview/collection-preview.compone
 
 import { selectCollectionForPreview } from "../../redux/shop/shop.selectors";
 
-import './collections-overview.styles.scss';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
 const CollectionsOverview = ({ collections }) => (
     <div className="collections-overview" >
